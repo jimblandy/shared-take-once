45520708d829ff05ab2d4cc75060f4ff64896abe
Jim Blandy <jimb@red-bean.com>
2022-08-03 22:36:06 -0700

    Cleanups. Tests.

--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,7 @@ pub mod non_sync {
             // `!Sync` because of the `UnsafeCell`, this is the only thread that
             // can see this value, so there are no other mutable references to
             // the `Inner`, so we can construct one here.
-            let inner: &mut Inner<T>  = unsafe { &mut *self.0 }.get_mut();
+            let inner: &mut Inner<T>  = unsafe { (*self.0).get_mut() };
             match inner.ref_count {
                 n if n > 0 => {
                     // Safety: ref_count is positive, so `value` is occupied.
@@ -108,7 +108,7 @@ pub mod non_sync {
             // `!Sync` because of the `UnsafeCell`, this is the only thread that
             // can see this value, so there are no other mutable references to
             // the `Inner`, so we can construct one here.
-            let inner: &mut Inner<T>  = unsafe { &mut *self.0 }.get_mut();
+            let inner: &mut Inner<T>  = unsafe { (*self.0).get_mut() };
             match inner.ref_count {
                 n if n > 1 => {
                     inner.ref_count -= 1;
@@ -141,10 +141,50 @@ pub mod non_sync {
             // `!Sync` because of the `UnsafeCell`, this is the only thread that
             // can see this value, so there are no other mutable references to
             // the `Inner`, so we can construct one here.
-            let inner: &mut Inner<T>  = unsafe { &mut *self.0 }.get_mut();
+            let inner: &mut Inner<T>  = unsafe { (*self.0).get_mut() };
             assert_ne!(inner.ref_count, 0);
             inner.ref_count += inner.ref_count.signum();
             SharedTakeOnce(self.0)
         }
     }
+}
+
+#[test]
+fn drop_two() {
+    use std::rc::Rc;
+    use non_sync::SharedTakeOnce;
+    
+    let counter = Rc::new(());
+
+    let handle1 = SharedTakeOnce::new(counter.clone());
+    assert_eq!(Rc::strong_count(&counter), 2);
+
+    let handle2 = handle1.clone();
+    assert_eq!(Rc::strong_count(&counter), 2);
+
+    drop(handle1);
+    assert_eq!(Rc::strong_count(&counter), 2);
+
+    drop(handle2);
+    assert_eq!(Rc::strong_count(&counter), 1);
+}
+
+#[test]
+fn take_one_drop_one() {
+    use std::rc::Rc;
+    use non_sync::SharedTakeOnce;
+    
+    let counter = Rc::new(());
+
+    let handle1 = SharedTakeOnce::new(counter.clone());
+    assert_eq!(Rc::strong_count(&counter), 2);
+
+    let handle2 = handle1.clone();
+    assert_eq!(Rc::strong_count(&counter), 2);
+
+    drop(handle1.take());
+    assert_eq!(Rc::strong_count(&counter), 1);
+
+    drop(handle2);
+    assert_eq!(Rc::strong_count(&counter), 1);
 }
