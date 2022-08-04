7ba39079e94c909ea4fdd715e662bfb8c0a1cad3
Jim Blandy <jimb@red-bean.com>
2022-08-03 22:36:41 -0700

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -59,7 +59,7 @@ let _ = promise.then2(&fulfill, &reject);
 
 pub mod non_sync {
     use std::{cell, mem};
-    
+
     struct Inner<T> {
         /// Positive reference count if occupied, negative reference count if taken.
         ref_count: isize,
@@ -82,7 +82,7 @@ pub mod non_sync {
             // `!Sync` because of the `UnsafeCell`, this is the only thread that
             // can see this value, so there are no other mutable references to
             // the `Inner`, so we can construct one here.
-            let inner: &mut Inner<T>  = unsafe { (*self.0).get_mut() };
+            let inner: &mut Inner<T> = unsafe { (*self.0).get_mut() };
             match inner.ref_count {
                 n if n > 0 => {
                     // Safety: ref_count is positive, so `value` is occupied.
@@ -91,9 +91,7 @@ pub mod non_sync {
                     inner.ref_count = -inner.ref_count;
                     Some(value)
                 }
-                n if n < 0 => {
-                    None
-                }
+                n if n < 0 => None,
                 _ => unreachable!("SharedTakeOnce should have been freed already"),
             }
             // `self` is dropped here, which adjusts the refcount and frees
@@ -108,7 +106,7 @@ pub mod non_sync {
             // `!Sync` because of the `UnsafeCell`, this is the only thread that
             // can see this value, so there are no other mutable references to
             // the `Inner`, so we can construct one here.
-            let inner: &mut Inner<T>  = unsafe { (*self.0).get_mut() };
+            let inner: &mut Inner<T> = unsafe { (*self.0).get_mut() };
             match inner.ref_count {
                 n if n > 1 => {
                     inner.ref_count -= 1;
@@ -141,7 +139,7 @@ pub mod non_sync {
             // `!Sync` because of the `UnsafeCell`, this is the only thread that
             // can see this value, so there are no other mutable references to
             // the `Inner`, so we can construct one here.
-            let inner: &mut Inner<T>  = unsafe { (*self.0).get_mut() };
+            let inner: &mut Inner<T> = unsafe { (*self.0).get_mut() };
             assert_ne!(inner.ref_count, 0);
             inner.ref_count += inner.ref_count.signum();
             SharedTakeOnce(self.0)
@@ -151,9 +149,9 @@ pub mod non_sync {
 
 #[test]
 fn drop_two() {
-    use std::rc::Rc;
     use non_sync::SharedTakeOnce;
-    
+    use std::rc::Rc;
+
     let counter = Rc::new(());
 
     let handle1 = SharedTakeOnce::new(counter.clone());
@@ -171,9 +169,9 @@ fn drop_two() {
 
 #[test]
 fn take_one_drop_one() {
-    use std::rc::Rc;
     use non_sync::SharedTakeOnce;
-    
+    use std::rc::Rc;
+
     let counter = Rc::new(());
 
     let handle1 = SharedTakeOnce::new(counter.clone());
